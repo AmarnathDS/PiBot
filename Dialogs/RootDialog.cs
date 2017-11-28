f60fe70c8f6154384092a5ca3d51ec9432eac519
Dontisriramulu Amarnath <amarnath.dontisriramulu@volvo.com>
2017-11-28 19:47:09 +0530

    fourth

--- Dialogs/RootDialog.cs
+++ Dialogs/RootDialog.cs
@@ -23,25 +23,27 @@ namespace PiBot.Dialogs
             activity.Text = activity.Text ?? string.Empty;
 
             // check if the user said reset
-            if (activity.Text.ToLowerInvariant().StartsWith("reset"))
+            if (activity.Text.ToLowerInvariant().StartsWith("Order"))
             {
                 // ask the user to confirm if they want to reset the counter
-                var options = new PromptOptions<string>(prompt: "Are you sure you want to reset the count?",
-                    retry: "Didn't get that!", speak: "Do you want me to reset the counter?",
-                    retrySpeak: "You can say yes or no!",
-                    options: PromptDialog.PromptConfirm.Options,
-                    promptStyler: new PromptStyler());
+                //var options = new PromptOptions<string>(prompt: "Are you sure you want to reset the count?",
+                //    retry: "Didn't get that!", speak: "Do you want me to reset the counter?",
+                //    retrySpeak: "You can say yes or no!",
+                //    options: PromptDialog.PromptConfirm.Options,
+                //    promptStyler: new PromptStyler());
 
-                PromptDialog.Confirm(context, AfterResetAsync, options);
+                //PromptDialog.Confirm(context, AfterResetAsync, options);
+                await context.SayAsync($"Please say the order number", $"Please say the order number", new MessageOptions() { InputHint = InputHints.ExpectingInput });
+                context.Wait(MessageReceivedAsync);
 
             }
-            else
+            else if (activity.Text.ToLowerInvariant().StartsWith("M"))
             {
                 // calculate something for us to return
-                int length = activity.Text.Length;
+                //int length = activity.Text.Length;
 
                 // increment the counter
-                this.count++;
+                //this.count++;
 
                 //Activity reply = activity.CreateReply($"{count}: You sent {activity.Text} which was {length} characters");
                 //reply.Speak = $"{count}: You said {activity.Text}";
@@ -51,8 +53,66 @@ namespace PiBot.Dialogs
 
 
                 // say reply to the user
-                await context.SayAsync($"{count}: You sent {activity.Text} which was {length} characters", $"{count}: You said {activity.Text} which was {length} characters", new MessageOptions() { InputHint = InputHints.ExpectingInput });
+                await context.SayAsync($"order {activity.Text} is deliverd to plant", $"order {activity.Text} is deliverd to plant", new MessageOptions() { InputHint = InputHints.ExpectingInput });
                 context.Wait(MessageReceivedAsync);
+            }
+            else if (activity.Text.ToLowerInvariant().StartsWith("A"))
+            {
+                // calculate something for us to return
+                //int length = activity.Text.Length;
+
+                // increment the counter
+                //this.count++;
+
+                //Activity reply = activity.CreateReply($"{count}: You sent {activity.Text} which was {length} characters");
+                //reply.Speak = $"{count}: You said {activity.Text}";
+                //reply.InputHint = InputHints.ExpectingInput;
+                //await context.PostAsync(reply);
+
+
+
+                // say reply to the user
+                await context.SayAsync($"order {activity.Text} is in transit", $"order {activity.Text} is in transit", new MessageOptions() { InputHint = InputHints.ExpectingInput });
+                context.Wait(MessageReceivedAsync);
+            }
+            else if (activity.Text.ToLowerInvariant().StartsWith("thanks"))
+            {
+                // calculate something for us to return
+                //int length = activity.Text.Length;
+
+                // increment the counter
+                //this.count++;
+
+                //Activity reply = activity.CreateReply($"{count}: You sent {activity.Text} which was {length} characters");
+                //reply.Speak = $"{count}: You said {activity.Text}";
+                //reply.InputHint = InputHints.ExpectingInput;
+                //await context.PostAsync(reply);
+
+
+
+                // say reply to the user
+                await
+                    context.SayAsync($"Happy to help", $"Happy to help",
+                        new MessageOptions() {InputHint = InputHints.ExpectingInput});
+                context.Wait(MessageReceivedAsync);
+            }
+            else if (activity.Text.ToLowerInvariant().StartsWith("Part"))
+            {
+                await context.SayAsync($"Please say the part number", $"Please say the part number", new MessageOptions() { InputHint = InputHints.ExpectingInput });
+                context.Wait(MessageReceivedAsync);
+            }
+            else if (activity.Text.ToLowerInvariant().Equals("2215656"))
+            {
+                await context.SayAsync($"Part Qty for part {activity.Text} is 5", $"Part Qty for part {activity.Text} is 5", new MessageOptions() { InputHint = InputHints.ExpectingInput });
+                context.Wait(MessageReceivedAsync);
+            }
+            else
+            {
+                await
+                       context.SayAsync($"Sorry did not get that. Please say again", $"Sorry did not get that. Please say again",
+                           new MessageOptions() { InputHint = InputHints.ExpectingInput });
+                context.Wait(MessageReceivedAsync);
+
             }
 
         }
